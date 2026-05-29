48465e5bf8b82a841d588f1ec567b7b1e71b292a
chittrapriya5796 <krcpriya@gmail.com>
2026-05-29 23:03:52 +0530

    URL updated

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ app.use(express.json())
 app.use(cookieParser());
 
 app.use(cors({
-    origin:'https://carbookingrent.netlify.app/',
+    origin: 'https://carbookingrent.netlify.app',
     credentials: true
-}))
+}));
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/cars', carRouter);
